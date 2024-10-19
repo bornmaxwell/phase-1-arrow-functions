594c31dbf08b30b25d78d80ddeac267eec3ec557
Maxwell Born <borncmaxwell@gmail.com>
2024-10-19 16:58:41 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,10 @@
+// divide function expression
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // square arrow function
+  const square = (num) => num * num;
+  
+  // add arrow function
+  const add = (a, b) => a + b;
